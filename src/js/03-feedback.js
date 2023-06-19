28637dcead7cc144981954f474554fbe2e9d7f9d
BozhiiKostya <kostyazap@gmail.com>
2023-06-20 00:19:25 +0300

    fixed js file after comments

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,31 +1,31 @@
 const formEl = document.querySelector('.feedback-form');
 const emailInput = document.querySelector('input[name="email"]');
 const messageInput = document.querySelector('textarea[name="message"]');
-
-const formData = {};
 const LOCALSTORAGE_KEY = 'feedback-form-state';
+let formData = {};
 
 formEl.addEventListener('input', onFormInput);
 formEl.addEventListener('submit', onFormSubmit);
-storageHaveValue();
+storageHaveValue(formData);
 
 function onFormInput(e) {
   formData[e.target.name] = e.target.value;
-  console.log(formData);
-
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
 }
 function onFormSubmit(e) {
   e.preventDefault();
-  localStorage.removeItem(LOCALSTORAGE_KEY);
-  e.currentTarget.reset();
+  if (formData.hasOwnProperty('email') && formData.hasOwnProperty('message')) {
+    console.log(formData);
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    e.target.reset();
+    formData = {};
+  }
 }
 
 function storageHaveValue() {
-  let parseLocalKey = {};
   if (localStorage.getItem(LOCALSTORAGE_KEY)) {
-    parseLocalKey = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-    messageInput.textContent = parseLocalKey.message || '';
-    emailInput.value = parseLocalKey.email || '';
+    formData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    messageInput.value = formData.message || '';
+    emailInput.value = formData.email || '';
   }
 }
